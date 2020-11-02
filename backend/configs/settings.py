f5beedfa14ca60768feb5b6cc2c8ee8a47140e4e
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-02 17:19:44 +0900

    setup Task model

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'goals.apps.GoalsConfig',
+    'tasks.apps.TasksConfig',
 ]
 
 MIDDLEWARE = [
--- backend/goalingball/tasks/models.py
+++ backend/goalingball/tasks/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+from goals.models import Goal
+from django.contrib.auth.models import User
+
+
+DAYS_OF_WEEK = (
+    ('NONE', 'None'),
+    ('MONDAY', 'Monday'),
+    ('TUESDAY', 'Tuesday'),
+    ('WEDNESDAY', 'Wednesday'),
+    ('THURSDAY', 'Thursday'),
+    ('FRIDAY', 'Friday'),
+    ('SATURDAY', 'Saturday'),
+    ('SUNDAY', 'Sunday'),
+)
+
+
+class Task(models.Model):
+    title = models.TextField(max_length=255, blank=False) 
+    goal = models.ForeignKey(
+        Goal,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    importance = models.FloatField(blank=True)
+    recurrent = models.BooleanField(default=False)
+    day_of_week = MultiSelectField(choices=DAYS_OF_WEEK, default='NONE')
+
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField()
+
+
+    def save(self, *args, **kwargs):
+        # For the first time
+        if not self.id: 
+            self.created_at = timezone.now()
+
+        # Upon save, update timestamps
+        self.updated_at = timezone.now()
+        
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "{} {}".format(self.id, self.title)
