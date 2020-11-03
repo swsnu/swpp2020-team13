525164a70e21782088731e5dd9fa9f4f9dcfd83c
jiyewise <kay2353@snu.ac.kr>
2020-11-04 06:54:16 +0900

    taskcrud branch init

--- backend/goalingball/tasks/views.py
+++ backend/goalingball/tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest, JsonResponse
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import ensure_csrf_cookie
+import json
+from json import JSONDecodeError
+from django.contrib.auth import authenticate, login, logout
+from .models import Goal, 
+from django.core.serializers.json import DjangoJSONEncoder
 
