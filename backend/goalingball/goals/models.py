630a7a8973286af4dd49e1cb40ea43d18a6089a3
jiyewise <kay2353@snu.ac.kr>
2020-11-04 12:29:49 +0900

    commit before pull

--- backend/goalingball/goals/models.py
+++ backend/goalingball/goals/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
 class Goal(models.Model):
     title = models.TextField(max_length=255, blank=False) 
     user = models.ForeignKey(
