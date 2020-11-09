7b78d59abfd8ecf35da78f8b70c9a78a19c0dd7d
jiyewise <kay2353@snu.ac.kr>
2020-11-09 16:39:20 +0900

    add sample task data to GoalBody

--- frontend/src/components/GoalBody/GoalBodyComponent.jsx
+++ frontend/src/components/GoalBody/GoalBodyComponent.jsx
@@ -1,23 +1,69 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Icon, Sidebar, Menu, Grid, List, Segment} from 'semantic-ui-react'
 import './GoalBody.css'
+import TaskBarComponent from '../TaskBar/TaskBarComponent'
 
-const GoalBodyComponent = (props) => {
+class GoalBodyComponent extends Component {
     // props have goal id, title, deadline, and tags
     // the tasks here are originally from backend
     // TODO: connect with redux
 
-    const sampleTaskList = []
+    state = {
+        selectedDate: null,
+        sampleTaskList:[
+            {
+                "id": 1,
+                "user": 1,
+                "goal": 2,
+                "title": "task_test_title_1",
+                "created_at": "2020-11-05 07:04:59",
+                "updated_at": "2020-11-05 07:04:59",
+                "deadline": "2020-11-09 05:38:20",
+                "importance": 3,
+                "DAYS_OF_WEEK": [
+                    "Monday",
+                ]
+            },
+            {
+                "id": 2,
+                "user": 1,
+                "goal": 2,
+                "title": "task_test_title_2",
+                "created_at": "2020-11-05 07:04:59",
+                "updated_at": "2020-11-05 07:04:59",
+                "deadline": "2020-11-10 05:38:20",
+                "importance": 5,
+                "DAYS_OF_WEEK": [
+                    "None",
+                ]
+            },
+            {
+                "id": 3,
+                "user": 1,
+                "goal": 2,
+                "title": "task_test_title_3",
+                "created_at": "2020-11-05 07:04:59",
+                "updated_at": "2020-11-05 07:04:59",
+                "deadline": "2020-11-10 05:38:20",
+                "importance": 4,
+                "DAYS_OF_WEEK": [
+                    "Friday",
+                ]
+            }
+        ]
+    }
 
     // const toTaskBar // map from sampleTaskList
+    // TODO: implement selectCertainTask function
 
-    const deadlineDate = (deadline) => {
+    deadlineDate = (deadline) => {
         return deadline.split(" ")[0]
     }
 
+    render() {
     return(
         <Segment className="GoalBodySegment">
-            <List className="GoalBodyList">
+            <List className="GoalBodyListTitle">
                 <List.Item>
                     <List.Content>
                         <List.Header>{props.title}</List.Header>
@@ -25,9 +71,12 @@ const GoalBodyComponent = (props) => {
                     </List.Content>
                 </List.Item>
             </List>
+            <List className="GoalBodyListwithTask">
+
+            </List>
         </Segment>
     )
-
+    }
 }
 
 export default GoalBodyComponent
