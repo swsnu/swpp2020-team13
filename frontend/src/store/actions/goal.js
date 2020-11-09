28ec3bf9b707722a2b141168defaa638fa67f1cf
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-09 13:36:38 +0900

    implement fileupload

--- frontend/src/store/actions/goal.js
+++ frontend/src/store/actions/goal.js
@@ -7,7 +7,7 @@ export const getAllGoal_ = (goals) => {
 
 export const getAllGoal = () => {
     return dispatch => {
-        return axios.get('/api/v1/goal/')
+        return axios.get('/api/v1/goals/')
         .then(res => dispatch(getAllGoal_(res.data)))
     }
 }
@@ -18,7 +18,7 @@ export const getGoal_ = (goal) => {
 
 export const getGoal = (id) => {
     return dispatch => {
-        return axios.get('/api/v1/goal/' + id + '/')
+        return axios.get('/api/v1/goals/' + id + '/')
         .then(res => {
             dispatch(getGoal_(res.data))
         })
@@ -29,17 +29,36 @@ export const addGoal_ = (goal) => {
     return {
         type: actionTypes.ADD_GOAL,
         title: goal.title,
-        photo: "test.goal.photo.url.com", 
-        deadline: goal.deadline,
-        tags: goal.tags
+        photo: goal.photo ? goal.photo : null, 
+        deadline: goal.deadline ? goal.deadline : null,
+        tags: goal.tags ? goal.tags : null
     }
 }
 
-export const addGoal = (goal) => {
-    return (dispatch) => {
-        return axios.post('/api/v1/goal/', goal)
-        .then(res=> dispatch(addGoal_(res.data)))
+export const addGoal = (formData, file) => async dispatch => {
+    const s3prefix = 'https://goalingball-test.s3.amazonaws.com/'
+    console.log("[DEBUG] addGoal formData: ", formData)
+    if (file) {
+        const res = await axios.get('/api/v1/uploads/')
+        console.log("[DEBUG] response.data in addGoal: ", res.data)
+
+        const response = await axios.put(res.data.url, file, {
+            headers: {
+                'Content-Type': 'image/jpeg' 
+            }
+        })
+
+        const imageUrl = s3prefix + res.data.key
+        console.log("[DEBUG] imageUrl: ", imageUrl)
+
+        formData['photo'] = imageUrl
+    } else {
+        console.log("[DEBUG] no file found in addGoal")
     }
+
+    const res = await axios.post('/api/v1/goals/', formData)
+    console.log("[DEBUG] res from server when adding goal: ", res.data)
+    dispatch(addGoal_(res.data))
 }
 
 export const deleteGoal_ = (id) => {
@@ -51,7 +70,7 @@ export const deleteGoal_ = (id) => {
 
 export const deleteGoal = (id) => {
     return dispatch => {
-        return axios.delete('/api/v1/goal/'+ id)
+        return axios.delete('/api/v1/goals/'+ id)
         .then(res => dispatch(deleteGoal_(id)))
     }
 }
@@ -68,7 +87,7 @@ export const editGoal_ = (goal) => {
 
 export const editGoal = (goal) => {
     return (dispatch) => {
-        return axios.put('/api/v1/goal/'+ goal.id + '/', goal)
+        return axios.put('/api/v1/goals/'+ goal.id + '/', goal)
         .then(res => dispatch(editGoal_(goal)))
     }
 }
--- frontend/src/store/reducers/authReducer.js
+++ frontend/src/store/reducers/authReducer.js
@@ -1,26 +1,25 @@
 import * as actionTypes from '../actions/types'
 
-const initialState = {
-  loginUser: null
+/*
+state: {
+  loginUser: {
+    id: number
+    username: string
+  }
 }
+*/
 
 export default function(state = null, action) {
   switch (action.type) {
     case actionTypes.SIGNUP_USER:
-      const newUser = {
-        id: action.id,
-        username: action.username
-      }
-      return {...state, loginUser: newUser}
+      return {...state, loginUser: action.payload }
+
     case actionTypes.LOGIN_USER:
-      const user = {
-        username: action.username
-      }
-      console.log("change loginuser state")
-      return {...state, loginUser: user}
-      // return action.payload  // user_id or user_info
+      return {...state, loginUser: action.payload}
+
     case actionTypes.LOGOUT_USER:
       return {...state, loginUser: null}
+      
     default:
       return state
   }
