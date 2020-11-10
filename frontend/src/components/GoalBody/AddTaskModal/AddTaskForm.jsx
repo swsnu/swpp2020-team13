c576e9fd2d117ef54a47d8e5b5d2a51be3fba0aa
jiyewise <kay2353@snu.ac.kr>
2020-11-11 00:36:59 +0900

    set AddTask form template

--- frontend/src/components/GoalBody/AddTaskModal/AddTaskForm.jsx
+++ frontend/src/components/GoalBody/AddTaskModal/AddTaskForm.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { Menu } from 'semantic-ui-react'
+import { Grid, Menu } from 'semantic-ui-react'
 import { Form, Button, Segment } from 'semantic-ui-react'
 import * as actionCreators from '../../../store/actions'
 import { useDispatch } from 'react-redux'
-
+import './AddTaskForm.css'
 const AddTaskForm = (props) => {
     const { register, handleSubmit, watch, errors } = useForm()
     
@@ -12,16 +12,36 @@ const AddTaskForm = (props) => {
         console.log(data)
     }
 
+
     const onError = (errors, e) => console.log(errors, e);
     watch() // watchAllFields
 
+    const options = [
+        { key: 'm', text: 'Male', value: 'male' },
+        { key: 'f', text: 'Female', value: 'female' },
+        { key: 'o', text: 'Other', value: 'other' },
+      ]
 
     return (
-        <Form >
-            <Segment>
-            <h2>Add Task</h2>
-            
-            </Segment>
+        <Form>
+            <Segment className="AddTaskForm">
+                <h3>&nbsp; Add Task</h3>
+                <Form.Group widths='equal'>
+                    <Form.Input label='Title' placeholder='Enter task title' />
+                    </Form.Group>
+                    <Form.Group>
+                    <Form.Select
+                        label='Day of Week'
+                        options={options}
+                        placeholder='Day of Week'
+                    />
+                    <Form.Input label='Deadline' placeholder='Last name' />
+                    </Form.Group>
+                    <Form.Group inline>
+                    <label>Importance</label>
+                    </Form.Group>
+                    <Form.Button className="AddTaskSubmitButton">Submit</Form.Button>                    
+                </Segment>
         </Form>
     )
 }
