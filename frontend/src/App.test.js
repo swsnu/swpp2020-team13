37299573d898b12871c0aa9747ede6a66f96cfdf
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-13 18:06:43 +0900

    not found module error

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
-import { connectRouter, ConnectedRouter } from 'connected-react-router';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { mount } from 'enzyme';
 import App from './App'
-import { getMockStore } from './test-utils/mocks'
-import { history } from './store'
+import store from './store/store'
+console.log("store: ", store)
+// import { getMockStore } from './test-utils/mocks'
+// import { history } from './store'
 
-const mockStore = getMockStore({ goals: [], selectedGoal: null });
+// const mockStore = getMockStore({ goals: [], selectedGoal: null });
 
-jest.mock('./pages/main/GoalListComponent', () => {
-  return jest.fn(props => {
-    return (
-      <div className="spyGoalList">
-        {props.title}
-      </div>);
-  });
-});
+// jest.mock('./pages/main/GoalListComponent', () => {
+//   return jest.fn(props => {
+//     return (
+//       <div className="spyGoalList">
+//         {props.title}
+//       </div>);
+//   });
+// });
 
 // jest.mock('./containers/ArticleList/AddArticle/AddArticle', ()=> {
 //   return jest.fn(props => {
@@ -27,19 +28,35 @@ jest.mock('./pages/main/GoalListComponent', () => {
 //   });
 // })
 
-describe('App', ()=> {
-  let app
 
-  beforeEach(()=> {
-    app = (
-        <App history={history}/>
-    )
-  })
 
-  it('should render', () => {
-    const component = shallow(app);
-    expect(component.find('.App').length).toBe(1);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
+
+  it('renders <App /> component', () => {
+    const component = mount(<Provider store={store}><App /></Provider>)
+    const wrapper = component.find('.App')
+    expect(wrapper).to.have.lengthOf(1)
+  })
+})
+
+// describe('App', ()=> {
+//   let app
+
+//   beforeEach(()=> {
+//     app = (
+//         <App history={history}/>
+//     )
+//   })
+
+//   it('should render', () => {
+//     const component = shallow(app);
+//     expect(component.find('.App').length).toBe(1);
+//   });
 
 //   it('should render login page', ()=> {
 //     history.push('/login')
@@ -59,7 +76,7 @@ describe('App', ()=> {
 //     expect(component.find('.spyAddArticle').length).toBe(1);
 //   });
 
-})
+// })
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
 //   ReactDOM.render(<App />, div);
