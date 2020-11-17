33119d16f5a08e3f7a623a4403290d061c217bb1
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-17 23:59:33 +0900

    change backend to use timestamp

--- backend/goalingball/goals/views.py
+++ backend/goalingball/goals/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
 from json import JSONDecodeError
+from datetime import datetime
 from django.contrib.auth import authenticate, login, logout
 from .models import Goal
 from django.core.serializers.json import DjangoJSONEncoder
@@ -63,9 +64,9 @@ def goalList(request):
 
         response_dict = {'id': new_goal.id, 'user': new_goal.user.id, 
                         'title': new_goal.title, 'photo': new_goal.photo, 
-                        'created_at': (new_goal.created_at).strftime('%Y-%m-%d %H:%M:%S'), 
-                        'updated_at' : (new_goal.updated_at).strftime('%Y-%m-%d %H:%M:%S'), 
-                        'deadline': (new_goal.deadline).strftime('%Y-%m-%d %H:%M:%S'), 
+                        'created_at': int(new_goal.created_at.timestamp()),
+                        'updated_at' : int(new_goal.updated_at.timestamp()), 
+                        'deadline': int(new_goal.deadline.timestamp()), 
                         'tags': new_goal.tags.names()[0]}
         # print("tags.names(): ", new_goal.tags.names())
         print("tags.names()[0]: ", new_goal.tags.names()[0])
@@ -105,7 +106,7 @@ def goalDetail(request, goal_id=""):
             goal_title = request.PUT['title']
             goal_photo = request.PUT['photo']
             goal_deadline = request.PUT['deadline']
-            goal_deadline = timezone.make_aware(datetime.strptime(goal_deadline, '%Y-%m-%d %H:%M:%S')) # JSON string for deadline should be '%Y-%m-%d %H:%M:%S'
+            goal_deadline = timezone.make_aware(datetime.fromtimestamp(int(goal_deadline))) # JSON string for deadline should be '%Y-%m-%d %H:%M:%S'
             if 'tags' in request.PUT: # tags should be added after an intance is created
                 goal_tags = request.PUT.getlist['tags']
                 goal.tags.set(*goal_tags, clear=True)
