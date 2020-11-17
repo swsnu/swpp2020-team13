a8c8adcb79931f992de3154dd106b9fc47a768df
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-17 22:52:44 +0900

    make photo in Goal nullable and change tags format

--- backend/goalingball/goals/views.py
+++ backend/goalingball/goals/views.py
@@ -39,14 +39,16 @@ def goalList(request):
 
         try:
             goal_title = request.POST['title']
-            goal_photo = request.POST['photo']
             goal_deadline = request.POST['deadline']
             goal_deadline = timezone.make_aware(datetime.fromtimestamp(int(goal_deadline)))
             # goal_deadline = timezone.make_aware(datetime.strptime(goal_deadline, '%Y-%m-%d %H:%M:%S')) # JSON string for deadline should be '%Y-%m-%d %H:%M:%S'
         except(KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
 
-        print("add goal photo: ", goal_photo)
+        if 'photo' in request.POST:
+            goal_photo = request.POST['photo']
+        else:
+            goal_photo = None
         new_goal = Goal(title=goal_title, photo=goal_photo, deadline=goal_deadline, user=request.user)
         new_goal.save() # goal_created_at and goal_updated_at is made when new goal is saved
         
@@ -61,8 +63,10 @@ def goalList(request):
                         'created_at': (new_goal.created_at).strftime('%Y-%m-%d %H:%M:%S'), 
                         'updated_at' : (new_goal.updated_at).strftime('%Y-%m-%d %H:%M:%S'), 
                         'deadline': (new_goal.deadline).strftime('%Y-%m-%d %H:%M:%S'), 
-                        'tags':[tag for tag in new_goal.tags.names()]}
-
+                        'tags': new_goal.tags.names()[0]}
+        # print("tags.names(): ", new_goal.tags.names())
+        print("tags.names()[0]: ", new_goal.tags.names()[0])
+        # print("tags: ", [tag for tag in new_goal.tags.names()])
         return JsonResponse(response_dict, status=201, safe=False)
     else:
         return HttpResponseNotAllowed(['GET', 'POST'])
@@ -82,7 +86,7 @@ def goalDetail(request, goal_id=""):
         response_dict = {'id': g.id, 'title': g.title, 'photo': g.photo, 
                         'user': g.user.id, 'created_at': g.created_at, 
                         'updated_at': g.updated_at, 'deadline': g.deadline, 
-                        'tags': [tag for tag in g.tags.names()], 'tasks': tasks}
+                        'tags': g.tags.names()[0], 'tasks': tasks}
         return JsonResponse(response_dict, safe=False, status=200)
             
     elif request.method == 'PUT' or request.method == 'PATCH':
