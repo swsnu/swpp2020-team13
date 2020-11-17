80ec01339ab2787d1eff78a126cd2d2ac418b2e8
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-11-17 10:30:35 +0900

    modify travis.yml

--- backend/goalingball/goals/views.py
+++ backend/goalingball/goals/views.py
@@ -43,6 +43,7 @@ def goalList(request):
         except(KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
 
+        print("add goal photo: ", goal_photo)
         new_goal = Goal(title=goal_title, photo=goal_photo, deadline=goal_deadline, user=request.user)
         new_goal.save() # goal_created_at and goal_updated_at is made when new goal is saved
         
