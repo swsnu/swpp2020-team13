88267c33e3095da4671a4067adfa87dc593a4076
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-12-09 17:51:27 +0900

    comment out STATICFIELS_DIRS

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -156,9 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(ROOT_DIR, 'goalingball', 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(ROOT_DIR, 'goalingball', 'static')
+# ]
 STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
 MEDIA_ROOT = os.path.join(ROOT_DIR, 'media')
 MEDIA_URL = '/media/'
