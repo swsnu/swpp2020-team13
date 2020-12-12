90d7f1c379fe22df12a188e66c241eae22b5044b
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-12-12 09:24:16 +0900

    fix DashBoardCards.test

--- frontend/src/pages/dashboard/DashBoardCards/DashBoardCards.test.js
+++ frontend/src/pages/dashboard/DashBoardCards/DashBoardCards.test.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { shallow } from 'enzyme';
-import { DashBoardCards } from './DashBoardCardsComponent';
+import { getMockStore } from '../../../test-utils/mocks'
+import DashBoardCards from './DashBoardCardsComponent';
+
+const stubInitialState = {
+    auth: null,
+}
+
+const mockStore = getMockStore(stubInitialState)
 
 describe('DashboardGoalcards', () => {
     it("should render, ", ()=>{
-        const component = shallow(<DashBoardCards/>)
+        const component = shallow(<DashBoardCards goalCardList={[]}/>)
         const wrapper = component.find(".DashBoardGoalCards")
         expect(wrapper.length).toBe(1)
     })
 
     it("should click next -> prevhandler, ", ()=>{
-        const component = shallow(<DashBoardCards/>)
+        const component = shallow(<DashBoardCards goalCardList={[]}/>)
         const wrapper_n = component.find(".DashBoardGoalCards .dashcardnext")
         wrapper_n.simulate("click")
         const wrapper = component.find(".DashBoardGoalCards .dashcardprev")
