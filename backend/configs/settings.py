786d5661224e21fe3782eec33df972216d7ac191
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-12-15 17:00:03 +0900

    change STATIC_ROOT

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(ROOT_DIR, 'goalingball', 'static')
 # ]
