6bb3389b8fe638d30bce6cf9687976efa0e1d176
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-12-17 17:28:23 +0900

    change users.urls

--- backend/goalingball/users/urls.py
+++ backend/goalingball/users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 
-from users.views import signup, login, logout, check_email, check_username, check_password, session
+from users import views
+# from users.views import signup, login, logout, check_email, check_username, check_password, session
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('check_email/', check_email, name='check_email'),
-    path('check_username/', check_username, name='check_username'),
-    path('check_password/', check_password, name='check_password'),
-    path('session/', session, name='session'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('check_email/', views.check_email, name='check_email'),
+    path('check_username/', views.check_username, name='check_username'),
+    path('check_password/', views.check_password, name='check_password'),
+    path('session/', views.session, name='session'),
     # path('<pk>/', detail, name='detail'),
 ]
