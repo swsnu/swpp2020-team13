2bf3222dffc213147379cd0a37ce0f6b610a2a85
Chanwoo Lee <chanwoo.lucio.lee@gmail.com>
2020-12-17 12:40:24 +0900

    comment out cors header

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
-    'corsheaders',
+    # 'corsheaders',
     'users.apps.UsersConfig',
     'goals.apps.GoalsConfig',
     'tasks.apps.TasksConfig',
